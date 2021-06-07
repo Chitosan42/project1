dadf0c73fd367162e4cdd199651a7d3bc44d15e4
Chitosan42 <WONG129@e.ntu.edu.sg>
2021-06-07 18:31:48 +0800

    Updated to button

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, {useState} from 'react';
+import { Text, View, StyleSheet, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  const [count, setCount] = useState(0)
+
+  function increment() {
+    setCount(count + 1)
+  }
+
+  function renderEncouragingText() {
+    if (count >= 20) {
+      return "Almost there!!!!!!!!!!"
+    } else if (count >= 10) {
+      return "Keep going!!!!!!"
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
+      <Text style={{fontSize: 60}}>{count}</Text>
+      <Button title="Increment" onPress={increment}></Button>
+      <Button title="Decrement" onPress={ () => setCount(count - 1) }></Button>
+
+      <TouchableOpacity style={styles.resetButton} onPress={ () => setCount(0) }>
+        <Text style={styles.resetButtonText}>Reset</Text>
+      </TouchableOpacity>
+
+      <Text>{renderEncouragingText()}</Text>
     </View>
   );
 }
@@ -14,8 +35,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
+  resetButton: {
+    backgroundColor: "red",
+    padding: 10, 
+    borderRadius: 10,
+  }, 
+  resetButtonText: {
+    color: "white"
+  }
 });
