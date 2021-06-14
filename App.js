a419cfe3820b80f7e7668b8e28035fb1c2534f18
russelltwl <63510251+russelltwl@users.noreply.github.com>
2021-06-14 22:08:22 +0800

    App icons for "Booking"

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
-
-
 import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome } from '@expo/vector-icons';
 
 import BookingScreen from './components/BookingScreen';
 import UploadScreen from './components/BookingScreen';
 import VerificationScreen from './components/VerificationScreen';
 import QRScreen from './components/BookingScreen';
-
 
 function SettingsScreen() {
   return (
@@ -24,12 +22,32 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'Booking') {
+              iconName = 'book';
+            } else if (route.name === 'Upload') {
+              iconName = 'upload';
+            } else if (route.name === 'Verification') {
+              iconName = 'check';
+            } else if (route.name === 'QR') {
+              iconName = 'qrcode';
+            }
+
+            return <FontAwesome name={iconName} size={size} color={color} />;
+            return <Octicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}>
         <Tab.Screen name="Booking" component={BookingScreen} />
         <Tab.Screen name="Upload" component={UploadScreen} />
         <Tab.Screen name="Verification" component={VerificationScreen} />
         <Tab.Screen name="QR" component={QRScreen} />
-        
       </Tab.Navigator>
     </NavigationContainer>
   );
