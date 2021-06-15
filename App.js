6676eb48a1ade35760727884ffa3886e590ded96
koufubobo <koot0004@e.ntu.edu.sg>
2021-06-15 14:35:29 +0800

    Navigation draft 1

--- App.js
+++ App.js
@@ -1,21 +1,130 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { FontAwesome } from '@expo/vector-icons';
 
-import BookingScreen from './components/BookingScreen';
-import UploadScreen from './components/BookingScreen';
-import VerificationScreen from './components/VerificationScreen';
-import QRScreen from './components/BookingScreen';
-
-function SettingsScreen() {
+//BOOKING
+function BookingScreen({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+      <Text>Book an Appointment</Text>
+      <Button
+        title="Choose Location"
+        onPress={() => navigation.navigate('Location')}
+      />
     </View>
   );
 }
+function LocationScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Location</Text>
+    </View>
+  );
+}
+const BookStack = createStackNavigator();//Booking stack
+function BookStackScreen() {
+  return (
+    <BookStack.Navigator>
+      <BookStack.Screen name="Booking" component={BookingScreen} />
+      <BookStack.Screen name="Location" component={LocationScreen} />
+    </BookStack.Navigator>
+  );
+}
+
+
+//QR 
+function QRScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Scan QR Code</Text>
+      <Button
+        title="Show PDF"
+        onPress={() => navigation.navigate('PDF')}
+      />
+    </View>
+  );
+}
+function QRcodeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>PDF</Text>
+    </View>
+  );
+}
+const QRStack = createStackNavigator();//QR stack
+function QRStackScreen() {
+  return (
+    <QRStack.Navigator>
+      <QRStack.Screen name="QR Code" component={QRScreen} />
+      <QRStack.Screen name="PDF" component={QRcodeScreen} />
+    </QRStack.Navigator>
+  );
+}
+
+
+//VERIFICATION
+function VerificationScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Verification Status</Text>
+      <Button
+        title="Pending"
+      />
+    </View>
+  );
+}
+function VerifyScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>PDF</Text>
+    </View>
+  );
+}
+const VerificationStack = createStackNavigator();//QR stack
+function VerificationStackScreen() {
+  return (
+    <VerificationStack.Navigator>
+      <VerificationStack.Screen name="Verification" component={VerificationScreen} />
+      <VerificationStack.Screen name="PDF" component={VerifyScreen} />
+    </VerificationStack.Navigator>
+  );
+}
+
+
+function UploadScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text></Text>
+      <Button
+        title="Upload Document"
+        onPress={() => navigation.navigate('PDF')}
+      />
+    </View>
+  );
+}
+function UploadedScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Document Uploaded</Text>
+    </View>
+  );
+}
+const UploadStack = createStackNavigator();//QR stack
+function UploadStackScreen() {
+  return (
+    <UploadStack.Navigator>
+      <UploadStack.Screen name="Upload" component={UploadScreen} />
+      <UploadStack.Screen name="PDF" component={UploadedScreen} />
+    </UploadStack.Navigator>
+  );
+}
+
+
+
 
 const Tab = createBottomTabNavigator();
 
@@ -23,19 +132,22 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
+      
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
+
             if (route.name === 'Booking') {
-              iconName = 'book';
-            } else if (route.name === 'Upload') {
-              iconName = 'upload';
-            } else if (route.name === 'Verification') {
-              iconName = 'check';
+               iconName = 'book';
             } else if (route.name === 'QR') {
               iconName = 'qrcode';
+            } else if (route.name === 'Verification') {
+              iconName = 'check';
+            } else if (route.name === 'Upload') {
+              iconName = 'upload';
             }
 
+            // You can return any component that you like here!
             return <FontAwesome name={iconName} size={size} color={color} />;
             return <Octicons name={iconName} size={size} color={color} />;
           },
@@ -44,10 +156,15 @@ export default function App() {
           activeTintColor: 'tomato',
           inactiveTintColor: 'gray',
         }}>
-        <Tab.Screen name="Booking" component={BookingScreen} />
-        <Tab.Screen name="Upload" component={UploadScreen} />
-        <Tab.Screen name="Verification" component={VerificationScreen} />
-        <Tab.Screen name="QR" component={QRScreen} />
+
+        <Tab.Screen name="Booking" component={BookStackScreen} />
+        <Tab.Screen name="Upload" component={UploadStackScreen} />
+        <Tab.Screen name="Verification" component={VerificationStackScreen} />
+        <Tab.Screen name="QR" component={QRStackScreen} />
+        
+        
+      
+      
       </Tab.Navigator>
     </NavigationContainer>
   );
