9f86db424b6c70e5ad2eb3044bbd162af13f14ce
Chitosan42 <WONG129@e.ntu.edu.sg>
2021-06-15 13:22:47 +0800

    Icons

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { FontAwesome } from '@expo/vector-icons';
+import * as React from "react";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome } from "@expo/vector-icons";
 
-import BookingScreen from './components/BookingScreen';
-import UploadScreen from './components/BookingScreen';
-import VerificationScreen from './components/VerificationScreen';
-import QRScreen from './components/BookingScreen';
+import BookingScreen from "./components/BookingScreen";
+import UploadScreen from "./components/BookingScreen";
+import VerificationScreen from "./components/VerificationScreen";
+import QRScreen from "./components/BookingScreen";
 
 function SettingsScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Settings!</Text>
     </View>
   );
@@ -26,14 +26,14 @@ export default function App() {
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
-            if (route.name === 'Booking') {
-              iconName = 'book';
-            } else if (route.name === 'Upload') {
-              iconName = 'upload';
-            } else if (route.name === 'Verification') {
-              iconName = 'check';
-            } else if (route.name === 'QR') {
-              iconName = 'qrcode';
+            if (route.name === "Booking") {
+              iconName = "book";
+            } else if (route.name === "Upload") {
+              iconName = "upload";
+            } else if (route.name === "Verification") {
+              iconName = "check";
+            } else if (route.name === "QR") {
+              iconName = "qrcode";
             }
 
             return <FontAwesome name={iconName} size={size} color={color} />;
@@ -41,9 +41,10 @@ export default function App() {
           },
         })}
         tabBarOptions={{
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'gray',
-        }}>
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+        }}
+      >
         <Tab.Screen name="Booking" component={BookingScreen} />
         <Tab.Screen name="Upload" component={UploadScreen} />
         <Tab.Screen name="Verification" component={VerificationScreen} />
